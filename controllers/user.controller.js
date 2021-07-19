628957efbcaac60f288e7747889ee83149cca594
Théo CHELIM <theochelim@gmail.com>
2021-07-19 23:23:23 +0200

    Add getAllUsers ans getUserInfos

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,23 @@
 const UserModel = require("../models/user.model");
+const ObjectID = require("mongoose").Types.ObjectId;
 
-module.exports.getAllUsers = async (req, res) => {};
+module.exports.getAllUsers = async (req, res) => {
+  const users = await UserModel.find().select("-password");
+  res.status(200).json(users);
+};
 
-module.exports.getUserInfos = async (req, res) => {};
+module.exports.getUserInfos = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(404).json("Invalid user");
+
+  UserModel.findById(req.params.id, (err, docs) => {
+    if (err) {
+      console.log("Find user error: " + err);
+    } else {
+      return res.status(200).json(docs);
+    }
+  }).select("-password");
+};
 
 module.exports.setUserBio = async (req, res) => {};
 
