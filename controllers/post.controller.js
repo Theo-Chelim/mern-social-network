4a7479b8356bcf224776a6e31ff520a1a53a0d48
Théo CHELIM <theochelim@gmail.com>
2021-07-23 22:29:10 +0200

    Add readPost controller function

---
+++ controllers/post.controller.js
@@ -0,0 +1,8 @@
+const postModel = require("../models/post.model");
+
+module.exports.readPost = (req, res) => {
+  PostModel.find((err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("Error to get data : " + err);
+  }).sort({ createdAt: -1 });
+};
