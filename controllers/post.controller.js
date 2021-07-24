46e2ce42086045511085873c8d3feca511c6d2ee
Théo CHELIM <theochelim@gmail.com>
2021-07-24 23:46:52 +0200

    Add createPost controller function

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -6,3 +6,22 @@ module.exports.readPost = (req, res) => {
     else console.log("Error to get data : " + err);
   }).sort({ createdAt: -1 });
 };
+
+
+module.exports.createPost = async (req, res) => {
+  const newPost = new postModel({
+    posterId: req.body.posterId,
+    message: req.body.message,
+    picture: "",
+    video: req.body.video,
+    likers: [],
+    comments: [],
+  });
+
+  try {
+    const post = await newPost.save();
+    return res.status(201).json(post);
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
