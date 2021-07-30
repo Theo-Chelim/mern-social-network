7e997db10460cfb9b7ee56919a224f72b22515f5
Théo CHELIM <theochelim@gmail.com>
2021-07-30 23:49:01 +0200

    Add comment controller function

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -116,3 +116,30 @@ module.exports.unlikePost = async (req, res) => {
   }
 };
 
+module.exports.commentPost = (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+  try {
+    return PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          comments: {
+            commenterId: req.body.commenterId,
+            commenterPseudo: req.body.commenterPseudo,
+            text: req.body.text,
+            timestamp: new Date().getTime(),
+          },
+        },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
